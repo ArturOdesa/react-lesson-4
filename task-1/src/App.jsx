050074afc0ab59e4f65d553faeeed1b09ab60bfe
Artur Stoianov <stoianovart@gmail.com>
2025-03-16 17:06:51 +0200

    adjust project, complete task 1

--- task-1/src/App.jsx
+++ task-1/src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function createRandomValue() {
+    return Math.floor(Math.random() * 101);
+  }
+  const [values, setValues] = useState([54, 23, 89, 67, 12, 41, 97, 35, 76, 5]);
+
+  const addRandomValue = () => {
+    
+    setValues([...values, createRandomValue()]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {values.map((value) => <li>{value}</li>)}
+      </ul>
+      <button onClick={addRandomValue}>Add random value</button>
     </>
   )
 }
